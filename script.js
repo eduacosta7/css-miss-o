ba65be7ad8add2153b88bb09607fdbb25536519a
edu <131039327+eduacosta7@users.noreply.github.com>
2024-08-13 10:18:08 -0300

    aa

---
+++ script.js
@@ -0,0 +1,81 @@
+const caixaPrincipal = document.querySelector('caixa-principal')
+const caixaPerguntas = document.querySelector('caixa-perguntas')
+const caixaAlternativas = document.querySelector('caixa-altrnativa')
+const caixaResultado = document.querySelector('caixa-resultado')
+const textoResultado = document.querySelector('texto-resultado')
+
+
+const perguntas = [
+    {
+        enunciado: "No âmbito social, você prefere",
+        alternativas: [
+
+            {
+                texto: "Alternativa1",
+                afirmação: "afirmação",
+            },
+
+            {
+                texto: "Alternativa2",
+                afirmação: "afirmação",
+            }
+        ]
+    },
+    {
+        enunciado: "No âmbito ambiental, você prefere",
+        alternativas: [
+
+            {
+                texto: "Alternativa3",
+                afirmação: "afirmação",
+            },
+        {
+
+                texto: "Alternativa4",
+                afirmação: "afirmação",
+        }
+        ]
+    },
+    {
+        enunciado: "No âmbito tecnológico, você prefere",
+        alternativas: [
+
+            {
+                texto: "Alternativa5",
+                afirmação: "afirmação",
+            },
+
+            {
+                texto: "Alternativa6",
+                afirmação: "afirmação",
+            }
+        ]
+    },
+]
+
+let atual = 0
+let perguntaAtual;
+
+function mostraPerguntas (){
+perguntaAtual =perguntas[atual];
+caixaPerguntas.textContent = perguntaAtual.enunciado;
+
+}
+
+mostraPerguntas();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+]
